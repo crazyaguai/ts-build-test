8b1f1ad1c28c8b2bd91ad41de6b34a25ba4b5413
crazyaguai <996397220@qq.com>
2020-09-05 18:20:55 +0800

    change

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,37 +8,42 @@ module.exports = {
     c1: true, // ？？？
   },
   extends: [
-    "eslint-config-airbnb-base",
-    "plugin:react/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "eslint-config-prettier",
+    'eslint-config-airbnb-base',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'eslint-config-prettier',
     // "eslint-config-airbnb-typescript",
     // "eslint-config-prettier/@typescript-eslint",
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     // ecmaVersion: 6,
   },
-  plugins: ["eslint-plugin-import", "@typescript-eslint","eslint-plugin-react"],
-  "settings": {
-    "import/extensions": [".tsx", ".ts",".jsx", ".js"],
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
+  plugins: [
+    'eslint-plugin-import',
+    '@typescript-eslint',
+    'eslint-plugin-react',
+  ],
+
+  settings: {
+    'import/extensions': ['.tsx', '.ts', '.jsx', '.js'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
   },
   rules: {
-    indent: ["error", 2],
-    "no-console": ["off"],
-    "linebreak-style": [0, "error", "windows"], // 换行配置 windows \r\n linux \n
+    indent: ['error', 2],
+    'no-console': ['off'],
+    'linebreak-style': [0, 'error', 'windows'], // 换行配置 windows \r\n linux \n
 
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
 
-    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
 
-    "import/extensions": ["off"]
+    'import/extensions': ['off'],
   },
-};
+}
